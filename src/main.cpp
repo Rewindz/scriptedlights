60b237727f4cdeff40e5f0e7886cf40030514acd
Rewindz <hx3er5now@gmail.com>
2025-12-07 12:07:05 -0500

    Got lua engine to run

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
 extern "C"
 {
@@ -9,35 +10,50 @@ extern "C"
 
 lua_State *L = nullptr;
 
-
-lua_State *initailizeLua()
+static int l_serial_printf(lua_State *L)
 {
-  lua_State *LL = luaL_newstate();
-
-  lua_pushcfunction(LL, luaopen_base);
-  lua_pushstring(LL, "");
-  lua_call(LL, 1, 0);
-
-  lua_pushcfunction(LL, luaopen_string);
-  lua_pushstring(LL, LUA_STRLIBNAME);
-  lua_call(LL, 1, 0);
-
-  lua_pushcfunction(LL, luaopen_table);
-  lua_pushstring(LL, LUA_TABLIBNAME);
-  lua_call(LL, 1, 0);
-
-  return LL;
+  int n = lua_gettop(L);
+  if(n < 1) return 0;
+  Serial.printf("Lua: %s\n", lua_tostring(L, 1));
+  return 0;
 }
+
+void initailizeLua()
+{
+  L = luaL_newstate();
+
+  lua_pushcfunction(L, luaopen_base);
+  lua_pushstring(L, "");
+  lua_call(L, 1, 0);
+
+  lua_pushcfunction(L, luaopen_string);
+  lua_pushstring(L, LUA_STRLIBNAME);
+  lua_call(L, 1, 0);
+
+  lua_pushcfunction(L, luaopen_table);
+  lua_pushstring(L, LUA_TABLIBNAME);
+  lua_call(L, 1, 0);
+
+  lua_pushcfunction(L, l_serial_printf);
+  lua_setglobal(L, "serial_print");
+}
+
+
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Boot");
-  L = initailizeLua();
-  if(!L) return;
+  Serial.printf("Boot\n");
+  initailizeLua();
+  if(!L) {
+    Serial.printf("Unable to initalize!\n");
+    return;
+  }
+
+  Serial.printf("Initialized!\n");
   
   
-  String script = "print('Hello from lua! 2 + 2 =' .. 2+2)";
+  String script = "serial_print('Hello from lua! 2 + 2 = ' .. 2+2)";
 
   int error = luaL_dostring(L, script.c_str());
 
